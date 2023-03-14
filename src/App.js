9ce95f11b170835c5a9202bfb087217063ae4446
Work Experience 2 <workexperience2@Works-MacBook-Pro.local>
2023-03-14 15:57:55 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+
+function App() {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const [formValues, setFormValues] = useState("");
+  const [showUserDetails, setShowUserDetails] = useState("");
+
+  const changeHandler = (event) => {
+    const ele = event.target;
+
+    if (ele.name  === 'firstName') {
+      setFirstName(ele.value);
+    }
+    if (ele.name  === 'lastName') {
+      setLastName(ele.value);
+    }
+}
+
+const submitHandler = (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  
+  if (firstName !== "" && lastName !== "") {
+    setFormValues({
+      firstName: firstName,
+      lastName: lastName
+    });
+    setShowUserDetails(true);
+  }
+}
+
+  return (
+    <div className="App">
+      <form>
+        <p>
+          First Name: <input type = "text" name='firstName' placeholder='First name' onChange={changeHandler}/>
+        </p>
+        <p>
+          Last Name: <input type = "text" name = 'lastName' placeholder='Last name' onChange={changeHandler}/>
+        </p>
+        <p>
+          <button className='btn-submit' onClick={submitHandler}>Submit</button>
+        </p>
+      </form>
+      {showUserDetails && (<Navigate to="/user-details" replace={true} state={formValues}/>)}
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/userDetails.js
@@ -0,0 +1,13 @@
+import { useLocation} from "react-router-dom"
+
+export const UserDetails = (props) => {
+    const {state} = useLocation();
+
+    return (
+        <div>
+            <h2>User Details</h2>
+            <p>First Name: {state.firstName}</p>
+            <p>Last Name: {state.lastName}</p>
+        </div>
+    )
+}
