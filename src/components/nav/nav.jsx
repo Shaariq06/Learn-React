8a4d9a43fd29db7f1f6ad8cb17ed277b7b2416c2
Shaariq06 <muhammadalishaariq@gmail.com>
2023-03-17 15:20:48 +0000

    mobile menu

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -23,10 +23,11 @@ const Nav = ({ links }) => {
         </div>
       </div>
       <div id="hamburger-icon" onClick={toggleMobileMenu}>
-        <div className="bar1"></div>
-        <div className="bar2"></div>
-        <div className="bar3"></div>
-        <ul className={`mobile-menu + ${isActive ? "open":""} `}>
+        <div className="bar1" style={{ display: !isActive ? "block" : "none" }}></div>
+        <div className="bar2" style={{ display: !isActive ? "block" : "none" }}></div>
+        <div className="bar3" style={{ display: !isActive ? "block" : "none" }}></div>
+        <div>X</div>
+        <ul className={`mobile-menu + ${isActive ? "open" : ""} `}>
           {links.map(({ link, label }, i) => (
             <li key={i}>
               <a href={link} className="nav-lower-section-link">
